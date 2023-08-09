4b24180f212a1ff298d8a3a5048478a48a004543
miguelangellerma <miguellangel770@gmail.com>
2023-08-08 20:32:37 -0500

    Added images carousel

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -1,4 +1,9 @@
+'use client';
 import Image from 'next/image';
+import { Button } from './button';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { ElementRef, useEffect, useMemo, useRef, useState } from 'react';
+import { throttle } from 'throttle-debounce';
 
 type Props = {
 	images: string[];
@@ -7,11 +12,64 @@ type Props = {
 };
 
 const Carousel = ({ images, imgWidth, imgHeight }: Props) => {
+	const ref = useRef<ElementRef<'div'>>(null);
+	const [selected, setSelected] = useState(0);
+
+	const onScroll = useMemo(
+		() =>
+			throttle(200, () => {
+				if (!ref.current) return;
+				setSelected(Math.round(ref.current.scrollLeft / ref.current.offsetWidth));
+			}),
+		[]
+	);
+
+	const slide = (direction: 'left' | 'right') => {
+		ref.current?.children[selected + (direction === 'left' ? -1 : 1)]?.scrollIntoView({
+			behavior: 'smooth',
+			block: 'nearest',
+			inline: 'start',
+		});
+	};
+
 	return (
-		<div className="w-full h-full overflow-auto snap-x snap-mandatory flex">
-			{images.map((x, i) => (
-				<Image key={i} src={x} width={imgWidth} height={imgHeight} alt="preview" className="shrink-0 top-0 inset-0" />
-			))}
+		<div className="relative">
+			<div ref={ref} className="w-full no-scrollbar overflow-auto snap-x snap-mandatory snap-always flex relative" onScroll={onScroll}>
+				{images.map((x, i) => (
+					<Image
+						key={i}
+						src={x}
+						width={imgWidth}
+						height={imgHeight}
+						alt="preview"
+						className="w-full h-auto shrink-0 snap-start object-cover"
+					/>
+				))}
+			</div>
+			{selected > 0 && (
+				<Button
+					onClick={() => slide('left')}
+					variant="ghost"
+					size="icon"
+					className="absolute bottom-1/2 left-1 translate-y-1/2 rounded-full bg-white/60 p-0 w-8 h-8 shadow-md hidden md:flex"
+				>
+					<ChevronLeft className="text-slate-300 text-black/40" />
+				</Button>
+			)}
+			{selected < images.length - 1 && (
+				<Button
+					onClick={() => slide('right')}
+					variant="ghost"
+					size="icon"
+					className="absolute bottom-1/2 right-1 translate-y-1/2 rounded-full bg-white/60 p-0 w-8 h-8 shadow-md hidden md:flex"
+				>
+					<ChevronRight className="text-slate-300 text-black/40" />
+				</Button>
+			)}
+			<div className="flex gap-1 absolute bottom-4 right-1/2 translate-x-1/2 bg-black/20 p-1 rounded-full">
+				{images.length > 1 &&
+					images.map((x, i) => <span key={i} className={`w-1.5 h-1.5 bg-white rounded-full ${selected !== i && 'opacity-40'}`}></span>)}
+			</div>
 		</div>
 	);
 };

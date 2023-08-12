7f58acb54cf182edb805b3a40d5ea5360a0ff9ec
miguelangellerma <miguellangel770@gmail.com>
2023-08-11 23:40:18 -0500

    Added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Noto_Sans_Mono } from 'next/font/google';
+import Footer from '@/components/Footer';
 
 const notoSansMono = Noto_Sans_Mono({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '700', '800'] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<body className={`${notoSansMono.className} min-w-[380px] bg-slate-900`}>
 				<Header />
 				{children}
+				<Footer />
 			</body>
 		</html>
 	);
---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import { Github, Linkedin } from 'lucide-react';
+import { Button } from './ui/button';
+
+const Footer = () => {
+	return (
+		<footer className="flex px-5 pt-20 pb-36 bg-slate-950">
+			<div className="max-w-screen-xl mx-auto w-full text-slate-500 text-sm text-center">
+				<div className="mb-4  gap-4">
+					<a href="https://linkedin.com/" target="_blank" className="mr-4">
+						<Button size="icon" tabIndex={-1}>
+							<Linkedin />
+						</Button>
+					</a>
+					<a href="https://github.com/m-ler" target="_blank">
+						<Button size="icon" tabIndex={-1}>
+							<Github />
+						</Button>
+					</a>
+				</div>
+				Made with Next.js
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;

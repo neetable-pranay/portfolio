10286e278be668e722a660ddb62fd8a36f02ec43
Pranayyeshaiah97 <pranayneetable@gmail.com>
2023-11-18 00:37:51 +0530

    header update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
 	return (
 		<header className="fixed top-0 left-0 right-0 z-10 flex px-4 py-4 bg-slate-900/80 backdrop-blur-md ">
 			<div className="flex items-center w-full max-w-screen-xl gap-4 mx-auto">
-				<span className="text-base font-black text-slate-200">mler.dev</span>
+				<span className="text-base font-black text-slate-200">pranay.dev</span>
 				<nav className="flex gap-2 ml-auto">
 					<Tooltip title="My LinkedIn">
 						<a href="https://www.linkedin.com/in/pranay-yeshaiah-830330119/" target="_blank">
